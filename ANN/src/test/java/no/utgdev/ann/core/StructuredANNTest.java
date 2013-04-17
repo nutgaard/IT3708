aa808e48ff4a37b3a08348732b09a4845ea731da
Nicklas Utgaard <nutgaard@gmail.com>
2013-04-17 09:49:39 +0200

    ANN tests

---
+++ ANN/src/test/java/no/utgdev/ann/core/StructuredANNTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.utgdev.ann.core;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nicklas
+ */
+public class StructuredANNTest {
+    
+    public StructuredANNTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAddLayer() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testGetLayers() {
+    }
+}
